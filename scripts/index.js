7e659e3e581f6b50c592d0b5eec7098de01a1030
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-06-03 10:41:01 -0300

    fix: fixed mobile navbar

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,29 @@
-const navbar = document.querySelector('.navbar');
-const mobileNavbar = document.querySelector('.navbar__mobile');
-const button = document.querySelector('.burguer');
+document.addEventListener('DOMContentLoaded', function() {
+  const navbar = document.querySelector('.navbar');
+  const mobileNavbar = document.querySelector('.navbar__mobile');
+  const button = document.querySelectorAll('.burguer');
+  const mainContent = document.querySelector('.main-content');
+  const mobileLinks = document.querySelectorAll('.mobile__links a');
 
-button.addEventListener('click', function () {
-  mobileNavbar.classList.toggle('active');
-});
+  window.addEventListener('scroll', function() {
+    if (window.pageYOffset > 0) {
+      navbar.classList.add('active');
+    } else {
+      navbar.classList.remove('active');
+    }
+  });
 
-window.addEventListener('scroll', function () {
-  if (this.window.pageYOffset > 0) return navbar.classList.add('active');
-  return navbar.classList.remove('active');
+  button.forEach(btn => {
+    btn.addEventListener('click', function() {
+      mobileNavbar.classList.toggle('active');
+      mainContent.classList.toggle('blurred');
+    });
+  });
+
+  mobileLinks.forEach(link => {
+    link.addEventListener('click', function() {
+      mobileNavbar.classList.remove('active');
+      mainContent.classList.remove('blurred');
+    });
+  });
 });
